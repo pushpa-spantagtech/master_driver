4521150c60e38e4d5bcf03dc6f62dfa9db3511e8
pushpa-spantagtech <pushpa.madasamy@spantagtech.com>
2026-08-21 15:56:59 +0530

    Reformatted code.

--- lib/features/location/controllers/location_controller.dart
+++ lib/features/location/controllers/location_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
